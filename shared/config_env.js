8b2fc57d57d947d58fd384bb84f55a5d7ea17a4f
Dharam Daxini <dharamdaxini@Dharams-Mac-mini.local>
2026-03-30 07:48:38 +0530

    feat: sophisticated 3D landing and fixed mobile redirect auth

--- shared/config_env.js
+++ shared/config_env.js
@@ -5,7 +5,8 @@ window.VIA_CONFIG = {
     projectId: "gen-lang-client-0530513864",
     storageBucket: "gen-lang-client-0530513864.firebasestorage.app",
     messagingSenderId: "783755173296",
-    appId: "1:783755173296:web:242974c84f464f979e8a75"
+    appId: "1:783755173296:web:242974c84f464f979e8a75",
+    databaseURL: "https://gen-lang-client-0530513864-default-rtdb.firebaseio.com/"
   },
   GEMINI: {
     apiKey: "AIzaSyB4ues4tuPmT_BVqTr_gzUQ5m0TPbLB5H4"
